b62676cf850212364669eabfb618680c40ae7871
Julian Rivera <juli.rivera.quintero@gmail.com>
2025-07-26 13:09:11 -0500

    feat: añadi las rutas de consumo para el primer motor

---
+++ src/services/engineOne.services.ts
@@ -0,0 +1,10 @@
+import { ESP32_URL } from "../constants/constants";
+
+export const engineOneServices = {
+  moveRight: async () => {
+    await fetch(`${ESP32_URL}/startRight1`);
+  },
+  moveLeft: async () => {
+    await fetch(`${ESP32_URL}/startLeft1`);
+  },
+};
