c68c3c1f1ad77ab1445d25412cbfde8925ddc5c3
Julian Rivera <juli.rivera.quintero@gmail.com>
2025-07-28 10:08:38 -0500

    feat: añado rutas para los presets de alturas

---
+++ src/services/tamanos.services.ts
@@ -0,0 +1,15 @@
+import { ESP32_URL } from "../constants/constants"
+
+
+
+export const tamanosServices = {
+    tamanoUno: async () => {
+        await fetch(`${ESP32_URL}/tamano1`)
+    },
+    tamanoDos: async () => {
+        await fetch(`${ESP32_URL}/tamano2`)
+    },
+    tamanoTrees: async () => {
+        await fetch(`${ESP32_URL}/tamano3`)
+    }
+}
