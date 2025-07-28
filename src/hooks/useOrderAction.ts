5bfdfd4071c1ea8ab3e13ed9d334cde78dcbf26b
Julian Rivera <juli.rivera.quintero@gmail.com>
2025-07-27 21:18:02 -0500

    feat: añadi la logica para el hook personalizado

---
+++ src/hooks/useOrderAction.ts
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { Snackbar } from "react-native-paper";
+import { OrderActionOptions } from "../types/order"; // o donde tengas el archivo
+
+export const useOrderAction = () => {
+  const [visible, setVisible] = useState(false);
+  const [message, setMessage] = useState("");
+  const [color, setColor] = useState<"green" | "red">("green");
+
+  const execute = async ({
+    action,
+    succesMessages,
+    errorMessages,
+  }: OrderActionOptions) => {
+    try {
+      await action();
+      if (succesMessages) {
+        setMessage(succesMessages);
+        setColor("green");
+        setVisible(true);
+      }
+    } catch (err) {
+      console.error("Order failed:", err);
+      setMessage(errorMessages ?? "Ocurrió un error");
+      setColor("red");
+      setVisible(true);
+    }
+  };
+
+  return { execute };
+};
