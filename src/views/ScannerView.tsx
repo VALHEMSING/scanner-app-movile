26359a1c00ab6ddd5b538a8c0f25c9558d41ab89
Julian Rivera <juli.rivera.quintero@gmail.com>
2025-07-28 21:59:14 -0500

    feat: adecuando mas cosas de UX

--- src/views/ScannerView.tsx
+++ src/views/ScannerView.tsx
@@ -17,10 +17,6 @@ import { ALTURAS_PRESETS } from "../constants/alturas.constants";
 import { tamanosServices } from "../services/tamanos.services";
 import { Header } from "../components/Header";
 
-
-
-
-
 const { width } = Dimensions.get("window");
 const isSmallScreen = width < 375; // iPhone SE y dispositivos pequeños
 
@@ -29,7 +25,7 @@ export const ScannerView = () => {
   const { execute } = useOrderAction();
 
   // --> Estados
-  const [altura, setAltura] = useState(0)
+  const [altura, setAltura] = useState(1);
   const [isScanning, setIsScanning] = useState(false);
   const [isPaused, setIsPaused] = useState(false);
   const [angulo, setAngulo] = useState<number>(0);
@@ -37,7 +33,7 @@ export const ScannerView = () => {
     visible: false,
     message: "",
   });
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
 
   // Refernacias
   const intervalRef = useRef<NodeJS.Timeout | null>(null);
@@ -45,8 +41,8 @@ export const ScannerView = () => {
   const directionRef = useRef(0); // Dirección actual (0 = detenido, 1 = subiendo, -1 = bajando)
 
   const lastSentValues = useRef({
-    altura: -1
-  })
+    altura: -1,
+  });
 
   // Sincronizar la referencia con el estado
   useEffect(() => {
@@ -163,204 +159,211 @@ export const ScannerView = () => {
   };
 
   const handleAlturaPreset = async (value: number) => {
-    setAltura(value)
-    switch(value) {
-      case 1: 
-      await tamanosServices.tamanoUno()
-      break;
-      case 2: 
-      await tamanosServices.tamanoDos()
-      break;
-      case 3: 
-      await tamanosServices.tamanoTres()
-      break;
-      default: 
-      console.warn('Altura invalida')
+    setAltura(value);
+    switch (value) {
+      case 1:
+        await tamanosServices.tamanoUno();
+        break;
+      case 2:
+        await tamanosServices.tamanoDos();
+        break;
+      case 3:
+        await tamanosServices.tamanoTres();
+        break;
+      default:
+        console.warn("Altura invalida");
     }
-  }
+  };
+
+  const handleReset = () => {};
 
   return (
     <ScrollView
-      className="flex-1 px-1"
-      contentContainerStyle={{ paddingBottom: 40,  }}
+      className="flex-1  bg-light-borderColor dark:bg-dark-borderColor"
+      contentContainerStyle={{
+        paddingBottom: 40,
+      }}
       showsVerticalScrollIndicator={false}
       keyboardShouldPersistTaps="handled"
       bounces={true}
     >
-              <Header />
-      
-      <Card className="mt-5  mx-16 dark:bg-dark-cardBackgraund">
-        <Card.Content className="border-2 rounded-xl dark:bg-dark-cardBackgraund">
-          <View className="items-center justify-center">
-            <Title>
-              <Text className="font-bold">Configuración WiFi</Text>
-            </Title>
-          </View>
-          <View className="p-4 border-2 rounded-xl bg-zinc-600">
-            <Text className="my-1">
-              <Text className="font-semibold">RED: </Text>
-              <Text>jfdifajiof</Text>
-            </Text>
-            <Text className="my-1">
-              <Text className="font-semibold">Contraseña:</Text>
-              <Text>1233456</Text>
-            </Text>
-          </View>
-        </Card.Content>
-      </Card>
-
-      <Card className="my-4 mx-16">
-        <Card.Content className="border-2 rounded-xl dark:bg-dark-cardBackgraund">
-          <Title className=" dark:text-dark-title m-auto font-bold items-center justify-center text-center">
-            Ajustes de placa de madera
-          </Title>
-          <Divider className="my-2" />
-          <View className="items-center my-4">
-            <Text>Presiona para mover la placa</Text>
-          </View>
-          <View className="flex-row justify-around">
-            <TouchableOpacity
-              className={`w-20 h-20 border-2 rounded-full items-center justify-center bg-red-600`}
-              onPress={() =>
-                execute({
-                  action: engineOneServices.moveLeft,
-                  succesMessages: "Motor a la izquierda",
-                  errorMessages: "Error al mover a la izquierda",
-                })
-              }
-              onPressOut={() =>
-                execute({
-                  action: engineOneServices.stop,
-                  succesMessages: "Motor detenido",
-                  errorMessages: "Error al detener el motor",
-                })
-              }
-            >
-              <Text className="text-white text-3xl">-</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              className={`w-20 h-20 border-2 rounded-full items-center justify-center bg-blue-400`}
-            >
-              <Text className="text-white text-3xl">+</Text>
-            </TouchableOpacity>
-          </View>
-        </Card.Content>
-      </Card>
-
-      {/* Card encargado del movimiento del sensor*/}
-      <Card className="my-4 mx-16">
-        <Card.Content className="border-2 rounded-xl p-4 dark:bg-dark-cardBackgraund">
-          <Title className=" m-auto font-bold text-lg dark:text-white">
-            Control de Ángulo (0° - 90°)
-          </Title>
-          <Divider className="my-2" />
-
-          <View className="items-center my-4">
-            <Text className="text-3xl font-bold dark:text-cyan-300">
-              {angulo}°
-            </Text>
-          </View>
-
-          <View className="flex-row justify-around ">
-            {/* Botón para disminuir */}
-            <TouchableOpacity
-              className={`w-20 h-20 border-2 rounded-full items-center justify-center 
-                ${angulo <= 0 ? "bg-gray-400" : "bg-red-500"}`}
-              disabled={angulo <= 0}
-              onPressIn={() => handlePressIn(-1)}
-              onPressOut={handlePressOut}
-              activeOpacity={0.7}
-            >
-              <Text className="text-white text-3xl">-</Text>
-            </TouchableOpacity>
-
-            {/* Botón para aumentar */}
-            <TouchableOpacity
-              className={`w-20 h-20 border-2 rounded-full items-center justify-center 
-                ${angulo >= 90 ? "bg-gray-400" : "bg-blue-500"}`}
-              disabled={angulo >= 90}
-              onPressIn={() => handlePressIn(1)}
-              onPressOut={handlePressOut}
-              activeOpacity={0.7}
-            >
-              <Text className="text-white text-3xl">+</Text>
-            </TouchableOpacity>
-          </View>
-        </Card.Content>
-      </Card>
-
-      <Card className="my-4 mx-16">
-        <Card.Content className="border-2 rounded-xl">
-          <Title className="font-bold m-auto">Alturas</Title>
-          <Divider />
-          <View className="my-3 m-auto flex-row gap-6">
-            {ALTURAS_PRESETS.map((preset, index) => (
-              <View key={index} className="rounded-full overflow-hidden">
-                <Button
-              mode={altura === preset.value ? "contained" : "outlined"}
-              onPress={() => handleAlturaPreset(preset.value)}
-              labelStyle={{fontSize: 16 }}
-              >
-                {preset.label}
-              </Button>
-              
-              </View>
-              
-            ))}
-          </View>
-
-          <Text className="m-auto">
-            Altura actual: {altura}
-          </Text>
-        </Card.Content>
-      </Card>
-
-      <Card className="mx-16 mt-4">
-        <Card.Content className="border-2 rounded-xl dark:bg-dark-cardBackgraund">
-          <Title className="font-bold m-auto">Otras opciones</Title>
-          <Divider className="my-2" />
-          <View className=" m-auto flex-row gap-3 my-2">
-            <Button mode="contained" icon="restart">
-              Reiniciar
-            </Button>
-            <Button
-              mode="contained"
-              icon={isScanning ? "close" : "camera"}
-              onPress={handleScantoggle}
-              buttonColor={isScanning ? "#D32F2F" : "#1976D2"}
-            >
-              {isScanning ? "Cancelar escaneo" : "Escanear"}
-            </Button>
-          </View>
-          <View className="m-auto gap-2 my-2 flex-row rounded-full">
-            <Button
-              mode="contained"
-              icon={isPaused ? "play" : "pause"}
-              onPress={handlePauseToggle}
-            >
-              {isPaused ? "Reanudar" : "Pausar"}
-            </Button>
-
-            <Button
-
-            mode="contained"
-            icon="stop"
-            buttonColor="red"
-            >
-              Detener
-            </Button>
-          </View>
-        </Card.Content>
-      </Card>
-
-      <Snackbar
-        visible={snackbar.visible}
-        onDismiss={() => setSnackbar({ ...snackbar, visible: false })}
-        duration={3500}
-        wrapperStyle={{ bottom: 80 }}
+      <View
+        className="p-4 rounded-3xl border-light-borderColor dark:border-dark-borderColor bg-light-bgBorder dark:bg-dark-bgBorder"
+        style={{
+          borderWidth: 8,
+          shadowColor: "#000",
+          shadowOffset: { width: 0, height: 4 },
+          shadowOpacity: 0.2,
+          shadowRadius: 6,
+          elevation: 6, // para Android
+        }}
       >
-        {snackbar.message}
-      </Snackbar>
+        <Header />
+
+        <Card className="mt-5  mx-8 dark:bg-dark-cardBackgraund">
+          <Card.Content className="border-2 rounded-xl dark:bg-dark-cardBackgraund">
+            <View className="items-center justify-center">
+              <Title>
+                <Text className="font-bold">Configuración WiFi</Text>
+              </Title>
+            </View>
+            <View className="p-4 border-2 rounded-xl bg-zinc-600">
+              <Text className="my-1">
+                <Text className="font-semibold">RED: </Text>
+                <Text>jfdifajiof</Text>
+              </Text>
+              <Text className="my-1">
+                <Text className="font-semibold">Contraseña:</Text>
+                <Text>1233456</Text>
+              </Text>
+            </View>
+          </Card.Content>
+        </Card>
+
+        <Card className="my-4 mx-5">
+          <Card.Content className="border-2 rounded-xl dark:bg-dark-cardBackgraund">
+            <Title className=" dark:text-dark-title m-auto font-bold items-center justify-center text-center ">
+              Ajustes de placa de madera
+            </Title>
+            <Divider className="my-2" />
+            <View className="items-center my-4">
+              <Text>Presiona para mover la placa</Text>
+            </View>
+            <View className="flex-row justify-around">
+              <TouchableOpacity
+                className={`w-20 h-20 border-2 rounded-full items-center justify-center bg-red-600`}
+                onPress={() =>
+                  execute({
+                    action: engineOneServices.moveLeft,
+                    succesMessages: "Motor a la izquierda",
+                    errorMessages: "Error al mover a la izquierda",
+                  })
+                }
+                onPressOut={() =>
+                  execute({
+                    action: engineOneServices.stop,
+                    succesMessages: "Motor detenido",
+                    errorMessages: "Error al detener el motor",
+                  })
+                }
+              >
+                <Text className="text-white text-3xl">-</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                className={`w-20 h-20 border-2 rounded-full items-center justify-center bg-blue-400`}
+              >
+                <Text className="text-white text-3xl">+</Text>
+              </TouchableOpacity>
+            </View>
+          </Card.Content>
+        </Card>
+
+        {/* Card encargado del movimiento del sensor*/}
+        <Card className="my-4 mx-5">
+          <Card.Content className="border-2 rounded-xl p-4 dark:bg-dark-cardBackgraund">
+            <Title className=" m-auto font-bold text-lg dark:text-white">
+              Control de Ángulo (0° - 90°)
+            </Title>
+            <Divider className="my-2" />
+
+            <View className="items-center mt-4">
+              <Text className="text-3xl font-bold dark:text-cyan-300">
+                {angulo}°
+              </Text>
+            </View>
+
+            <View className="flex-row justify-around ">
+              {/* Botón para disminuir */}
+              <TouchableOpacity
+                className={`w-20 h-20 border-2 rounded-full items-center justify-center 
+                ${angulo <= 0 ? "bg-gray-400" : "bg-red-500"}`}
+                disabled={angulo <= 0}
+                onPressIn={() => handlePressIn(-1)}
+                onPressOut={handlePressOut}
+                activeOpacity={0.7}
+              >
+                <Text className="text-white text-3xl">-</Text>
+              </TouchableOpacity>
+
+              {/* Botón para aumentar */}
+              <TouchableOpacity
+                className={`w-20 h-20 border-2 rounded-full items-center justify-center 
+                ${angulo >= 90 ? "bg-gray-400" : "bg-blue-500"}`}
+                disabled={angulo >= 90}
+                onPressIn={() => handlePressIn(1)}
+                onPressOut={handlePressOut}
+                activeOpacity={0.7}
+              >
+                <Text className="text-white text-3xl">+</Text>
+              </TouchableOpacity>
+            </View>
+          </Card.Content>
+        </Card>
+
+        <Card className="my-4 mx-5">
+          <Card.Content className="border-2 rounded-xl">
+            <Title className="font-bold m-auto">Alturas</Title>
+            <Divider />
+            <View className="my-3 m-auto flex-row gap-6">
+              {ALTURAS_PRESETS.map((preset, index) => (
+                <View key={index} className="rounded-full overflow-hidden">
+                  <Button
+                    mode={altura === preset.value ? "contained" : "outlined"}
+                    onPress={() => handleAlturaPreset(preset.value)}
+                    labelStyle={{ fontSize: 16 }}
+                  >
+                    {preset.label}
+                  </Button>
+                </View>
+              ))}
+            </View>
+
+            <Text className="m-auto">Altura actual: {altura}</Text>
+          </Card.Content>
+        </Card>
+
+        <Card className="mx-5 mt-4">
+          <Card.Content className="border-2 rounded-xl dark:bg-dark-cardBackgraund">
+            <Title className="font-bold m-auto">Otras opciones</Title>
+            <Divider className="my-2" />
+            <View className=" m-auto flex-row gap-3 my-2">
+              <Button mode="contained" icon="restart">
+                Reiniciar
+              </Button>
+              <Button
+                mode="contained"
+                icon={isScanning ? "close" : "camera"}
+                onPress={handleScantoggle}
+                buttonColor={isScanning ? "#D32F2F" : "#1976D2"}
+              >
+                {isScanning ? "Cancelar escaneo" : "Escanear"}
+              </Button>
+            </View>
+            <View className="m-auto gap-2 my-2 flex-row rounded-full">
+              <Button
+                mode="contained"
+                icon={isPaused ? "play" : "pause"}
+                onPress={handlePauseToggle}
+              >
+                {isPaused ? "Reanudar" : "Pausar"}
+              </Button>
+
+              <Button mode="contained" icon="stop" buttonColor="red">
+                Detener
+              </Button>
+            </View>
+          </Card.Content>
+        </Card>
+
+        <Snackbar
+          visible={snackbar.visible}
+          onDismiss={() => setSnackbar({ ...snackbar, visible: false })}
+          duration={3500}
+          wrapperStyle={{ bottom: 80 }}
+        >
+          {snackbar.message}
+        </Snackbar>
+      </View>
     </ScrollView>
   );
 };
